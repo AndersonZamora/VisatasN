a5d79496e504a22ec1e312438bb3878bf4d0d19b
Anderson <anderson_coral@outlook.com>
2019-11-27 11:36:40 -0500

    Version 1

---
+++ Vistas/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vistas.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuarip
+        public ActionResult Lista()
+        {
+            return View();
+        }
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+        public ActionResult Editar()
+        {
+            return View();
+        }
+    }
+}
